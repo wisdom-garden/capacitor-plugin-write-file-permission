b06989cdffb5630251d78449bd177f9a06245863
liupeixin <fengyi.mail@gmail.com>
2023-12-12 17:55:42 +0800

    feat: enhance logic

--- android/src/main/java/com/wisdomgarden/mobile/WriteFilePermission.java
+++ android/src/main/java/com/wisdomgarden/mobile/WriteFilePermission.java
@@ -35,7 +35,7 @@ public class WriteFilePermission extends Plugin {
     }
 
     private boolean hasPermission() {
-        if (this.useManagerExternalStorage && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        if (this.useManagerExternalStorage && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R && android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.TIRAMISU) {
             return Environment.isExternalStorageManager();
         } else {
             return hasPermission(permissionName);
